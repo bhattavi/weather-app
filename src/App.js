dfd4ed4b844a73119a97ebaeb08907eb4b47848c
abhinav <bhattabhinav@github.com>
2019-11-19 00:42:26 -0500

    intial mess

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const APPID = "93554df041fc50be24dc41455ad747c1"
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      isLoading: true,
+      temp: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined
+
+    }
+  }
+
+  getWeather = async (event) => {
+    event.preventDefault()
+    const city = event.target.elements.city.value
+    const country = event.target.elements.country.value
+    const reponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APPID}&units=metric`)
+    const data = await reponse.json()
+
+    this.setState({
+
+      isLoading: false,
+      temp: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description
+
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+
+        <nav className="navbar navbar-light bg-danger">
+          <a className="navbar-brand" style={{color : "white"}} href="#">Abhinav's Weather App ©</a>
+        </nav>
+
+        <div className="container">
+          <div className="jumbotron">
+            <h1 className="display-4">Stay Informed No Matter What</h1>
+            <p style={{fontStyle: "italic"}} className="lead">Wherever you go, no matter what the weather, always bring your own sunshine ☀️.</p>
+          </div>
+          <form style={{ marginTop: "0px" }} onSubmit={this.getWeather}>
+            <div className="row">
+              <div className="col">
+                <input type="text" name="city" placeholder="City" required></input>
+              </div>
+              <div className="col">
+                <input type="text" name="country" placeholder="Country" required></input>
+              </div>
+            </div>
+              <br/>
+         
+            <button style={{ marginTop: "3px" }} className="btn btn-danger btn-lg" >🌈⛅️Get Weather🌧❄️</button>
+          </form>
+          <div className="list-group">
+            {!this.state.isLoading && <h1 className="list-group-item, alert-danger" style={{ marginTop: "50px" }}>City: {this.state.city}</h1>}
+            {!this.state.isLoading && <h1 className="list-group-item, alert-danger">Country: {this.state.country}</h1>}
+            {!this.state.isLoading && <h3 className="list-group-item, alert-danger">Tempreture: {this.state.temp}℃</h3>}
+            {!this.state.isLoading && <h3 className="list-group-item, alert-danger">Humidity: {this.state.humidity}</h3>}
+            {!this.state.isLoading && <p className="list-group-item, alert-danger">Description: {this.state.description}</p>}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
